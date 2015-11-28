115d45039bbbe7734918e0d498671d266c375179
Prasanth <pmathial@buffalo.edu>
2015-11-28 18:37:26 -0500

    Tweets tagged using Alchemy API

--- scripts/TwSearch.py
+++ scripts/TwSearch.py
@@ -8,6 +8,7 @@ import json
 import os.path
 import tweepy
 import codecs
+import alchemy
 
 #Global variables required through out the app
 #Access Keys and tokens
@@ -18,6 +19,7 @@ access_token_secret = None
 tweepyAPI = None
 tweetDict = {}
 months = {v: k for k,v in enumerate(calendar.month_abbr)}
+taggedDict = {}
 
 # Setup various access keys and tokens from the user given input file 
 def setupAuthKeys(authFile):
@@ -47,22 +49,23 @@ def setupTweepyAPI():
 def getTweets(queryString, fromDate, toDate):
 	global tweepyAPI
 	tweetResult = []
-	year = '2015-11-'	
+	year = '2015-11-'
+	count_max=10
 	# Querying for English tweets
-	tweets = tweepyAPI.search(q=queryString,include_entities=True,lang='en', since=year+fromDate,until=year+toDate,count=100)
+	tweets = tweepyAPI.search(q=queryString,include_entities=True,lang='en', since=year+fromDate,until=year+toDate,count=count_max)
 	tweetResult += tweets['statuses']
 
-	tweets = tweepyAPI.search(q=queryString,include_entities=True,lang='fr', since=year+fromDate,until=year+toDate,count=100)
+	tweets = tweepyAPI.search(q=queryString,include_entities=True,lang='fr', since=year+fromDate,until=year+toDate,count=count_max)
 	tweetResult += tweets['statuses']
 		
 	# Querying for german tweets
 	#Berlin- 52.533028,13.388744,20km
-	tweets = tweepyAPI.search(q=queryString,include_entities=True, lang='de', since=year+fromDate,until=year+toDate,count=100)#,geocode="51.426129,10.267352,1500km")
+	tweets = tweepyAPI.search(q=queryString,include_entities=True, lang='de', since=year+fromDate,until=year+toDate,count=count_max)#,geocode="51.426129,10.267352,1500km")
 	tweetResult += tweets['statuses']
 
 	# Querying for Russian tweets
 	#Moscow - 55.601328,38.034511,500km
-	tweets = tweepyAPI.search(q=queryString,include_entities=True,lang='ru', since=year+fromDate,until=year+toDate,count=100)#,geocode="60.477265, 92.614585,1500km")
+	tweets = tweepyAPI.search(q=queryString,include_entities=True,lang='ru', since=year+fromDate,until=year+toDate,count=count_max)#,geocode="60.477265, 92.614585,1500km")
 	tweetResult += tweets['statuses']
 
 	return tweetResult
@@ -89,6 +92,26 @@ def processUrls(tweet):
 					urls.append(url['expanded_url'])
 	return urls
 
+# Extract entities from tagged data
+def processEntities(taggedData):
+	entities = []
+	if taggedData.get('entities'):
+		entity = taggedData['entities'];
+		if entity and len(entity) > 0:
+			for e in entity:
+				if e['type'] != 'TwitterHandle' and e['type'] != 'Hashtag':
+					entities.append(e['text'])
+	return entities
+
+# Extract concepts from tagged data
+def processConcepts(taggedData):
+	concepts = []
+	if taggedData.get('concepts'):
+		concept = taggedData['concepts'];
+		if concept and len(concept) > 0:
+			for e in concept:
+				concepts.append(e['text'])
+	return concepts
 
 def processDate(timeStamp):
 	#   
@@ -104,8 +127,9 @@ def processDate(timeStamp):
 #Process given tweet and extract all info
 def processTweet(tweet):
 	global tweetDict
+	global taggedDict
 	textKeyName = 'text_'
-   	twt = {}
+	twt = {}
 	twt['id'] = tweet['id_str']
 	twt['lang'] = tweet['lang']
 	textKeyName += twt['lang']
@@ -117,8 +141,18 @@ def processTweet(tweet):
 	twt['favorite_count'] = tweet['favorite_count']
 	#twt[''] = tweet['user']['location']
 	#twt['geo']=tweet['geo']
-   	tweetDict[twt['id']] = twt
 
+	if twt['lang'] == 'en':
+		data = alchemy.tagContent('bcae79f944a5cb0db0c70a8951776c3086478d09',tweet['text'].encode('utf-8'))
+		tag = {}
+		tag['id'] = tweet['id_str']
+		tag['concepts'] = data['concepts']
+		tag['entities'] = data['entities']
+		twt['concepts'] = processConcepts(data)
+		twt['entities'] = processEntities(data)
+		taggedDict[tag['id']] = tag
+
+	tweetDict[twt['id']] = twt
 
 def writeTweetsToFile(queryString, fromDate, toDate, tweets):
 	global tweetDict
@@ -136,7 +170,18 @@ def writeTweetsToFile(queryString, fromDate, toDate, tweets):
 	raw = codecs.open(outFileName+"_raw"+".json",'w','utf-8')
 	print(tweets,file=raw)
 	raw.close()
-
+	global taggedDict
+	outFileName = '_'.join(["tweets",queryString.replace(' ','_'), fromDate, toDate])
+	f = codecs.open(outFileName+"_tagged.json",'w','utf-8')
+	f.write('[')
+	tlist = taggedDict.values()
+	tlen = len(tlist)-1
+	for tweet in tlist:
+		f.write(json.dumps(tweet))
+		if(tlist.index(tweet) != tlen):
+			f.write(",")
+	f.write("]")
+	f.close()
 
 #Entry point
 def main():		
